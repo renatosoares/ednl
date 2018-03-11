b0b01ae26792795be896432b7fd19db4666e13f9
Renato Soares <renatosoarespro@hotmail.com>
2018-03-11 13:00:34 -0300

    AVL tree methods rotations test unit

--- tests/AVLTreeTest.php
+++ tests/AVLTreeTest.php
@@ -10,6 +10,9 @@ namespace EDNL\TREE;
 
 class AVLTreeTest extends BaseBSTTest
 {
+    /**
+     *
+     */
     public function testDelete()
     {
         /** @var AVLTree $tree */
@@ -29,6 +32,9 @@ class AVLTreeTest extends BaseBSTTest
         $this->testTreeBSTProperties($tree->root);
     }
 
+    /**
+     *
+     */
     public function testInsert()
     {
         /** @var AVLTree $tree */
@@ -62,9 +68,193 @@ class AVLTreeTest extends BaseBSTTest
         $this->testTreeBSTProperties($tree->root);
     }
 
-    public function testInsertTree()
+    /**
+     * @throws \Exception
+     */
+    public function testRotateLeft()
     {
-        $avl = new AvlTree();
+        /** @var Node  $root */
+        $root = new AVLNode(4, null, null, null);
+
+        /** @var Node $rightChild */
+        $rightChild = new AVLNode(6, $root, null, null);
+        $root->right = $rightChild;
+
+        /** @var Node $leftGrandChild */
+        $leftGrandChild = new AVLNode(5, $rightChild, null, null);
+
+        /** @var Node $rightGrandChild */
+        $rightGrandChild = new AVLNode(7, $rightChild, null, null);
+        $rightChild->left = $leftGrandChild;
+        $rightChild->right = $rightGrandChild;
+
+        /** @var AVLTree $tree */
+        $tree = new AVLTree();
+
+        /**
+         * teste método protected AVLTree::rotateLeft
+         */
+        $reflection = new \ReflectionClass(get_class($tree));
+        $method = $reflection->getMethod('rotateLeft');
+        $method->setAccessible(true);
+        /** @var Node $rotated */
+        $rotated = $method->invokeArgs($tree, [$root]);
+
+        $this->assertEquals((int) $rotated->value, 6);
+        $this->assertEquals((int) $rotated->left->value, 4);
+        $this->assertEquals((int) $rotated->right->value, 7);
+        $this->assertEquals((int) $rotated->left->right->value, 5);
+
+        $this->assertNull($rotated->parent);
+        $this->assertEquals($rotated->left->parent->value, $rotated->value);
+        $this->assertEquals($rotated->right->parent->value, $rotated->value);
+        $this->assertEquals($rotated->left->right->parent->value, $rotated->left->value);
+    }
+
+    /**
+     *
+     * @throws \ReflectionException
+     */
+    public function testRotateRight()
+    {
+        /** @var Node $root */
+        $root = new AVLNode(8, null, null, null);
+
+        /** @var Node $leftChild */
+        $leftChild = new AVLNode(6, $root, null, null);
+
+        $root->left = $leftChild;
+
+        /** @var Node $leftGrandChild */
+        $leftGrandChild = new AVLNode(5, $leftChild, null, null);
+
+        /** @var Node $rightGrandChild */
+        $rightGrandChild = new AVLNode(7, $leftChild, null, null);
+        $leftChild->left = $leftGrandChild;
+        $leftChild->right = $rightGrandChild;
+
+        /** @var AVLTree $tree */
+        $tree = new AVLTree();
+
+        /**
+         * teste método protected AVLTree::rotateRight
+         */
+        $reflection = new \ReflectionClass(get_class($tree));
+        $method = $reflection->getMethod('rotateRight');
+        $method->setAccessible(true);
+        /** @var Node $rotated */
+        $rotated = $method->invokeArgs($tree, [$root]);
+
+        $this->assertEquals((int) $rotated->value, 6);
+        $this->assertEquals((int) $rotated->left->value, 5);
+        $this->assertEquals((int) $rotated->right->value, 8);
+        $this->assertEquals((int) $rotated->right->left->value, 7);
+
+        $this->assertNull($rotated->parent);
+        $this->assertEquals($rotated->left->parent->value, $rotated->value);
+        $this->assertEquals($rotated->right->parent->value, $rotated->value);
+        $this->assertEquals($rotated->right->left->parent->value, $rotated->right->value);
+    }
+
+    /**
+     *
+     * @throws \ReflectionException
+     */
+    public function testDoubleRotateRightLeft()
+    {
+        /** @var Node $root */
+        $root = new AVLNode(5, null, null, null);
+
+        /** @var Node $rightChild */
+        $rightChild = new AVLNode(8, $root, null, null);
+
+
+        $root->right = $rightChild;
+
+        /** @var Node $leftGrandChild */
+        $leftGrandChild = new AVLNode(7, $rightChild, null, null);
+
+        /** @var Node $rightGrandChild */
+        $rightGrandChild = new AVLNode(10, $rightChild, null, null);
+
+        $rightChild->left = $leftGrandChild;
+        $rightChild->right = $rightGrandChild;
+
+        /** @var AVLTree $tree */
+        $tree = new AVLTree();
+
+        /**
+         * teste método protected AVLTree::doubleRotateRightLeft
+         */
+        $reflection = new \ReflectionClass(get_class($tree));
+        $method = $reflection->getMethod('doubleRotateRightLeft');
+        $method->setAccessible(true);
+        /** @var Node $rotated */
+        $rotated = $method->invokeArgs($tree, [$root]);
+
+        $this->assertEquals((int) $rotated->value, 7);
+        $this->assertEquals((int) $rotated->left->value, 5);
+        $this->assertEquals((int) $rotated->right->value, 8);
+        $this->assertEquals((int) $rotated->right->right->value, 10);
+
+        $this->assertNull($rotated->parent);
+        $this->assertEquals($rotated->left->parent->value, $rotated->value);
+        $this->assertEquals($rotated->right->parent->value, $rotated->value);
+        $this->assertEquals($rotated->right->right->parent->value, $rotated->right->value);
+
+        $tree->printTree();
+    }
+
+    /**
+     *
+     * @throws \ReflectionException
+     */
+    public function testDoubleRotateLeftRight()
+    {
+        /** @var Node $root */
+        $root = new AVLNode(5, null, null, null);
+
+        /** @var Node $leftChild */
+        $leftChild = new AVLNode(3, $root, null, null);
+        $root->left = $leftChild;
+
+        /** @var Node $leftGrandChild */
+        $leftGrandChild = new AVLNode(1, $leftChild, null, null);
+
+        /** @var Node $rightGrandChild */
+        $rightGrandChild = new AVLNode(4, $leftChild, null, null);
+        $leftChild->left = $leftGrandChild;
+        $leftChild->right = $rightGrandChild;
+
+        /** @var AVLTree $tree */
+        $tree = new AVLTree();
+
+        /**
+         * teste método protected AVLTree::doubleRotateLeftRight
+         */
+        $reflection = new \ReflectionClass(get_class($tree));
+        $method = $reflection->getMethod('doubleRotateLeftRight');
+        $method->setAccessible(true);
+        /** @var Node $rotated */
+        $rotated = $method->invokeArgs($tree, [$root]);
+
+        $this->assertEquals((int) $rotated->value, 4);
+        $this->assertEquals((int) $rotated->left->value, 3);
+        $this->assertEquals((int) $rotated->right->value, 5);
+        $this->assertEquals((int) $rotated->left->left->value, 1);
+
+        $this->assertNull($rotated->parent);
+        $this->assertEquals($rotated->left->parent->value, $rotated->value);
+        $this->assertEquals($rotated->right->parent->value, $rotated->value);
+        $this->assertEquals($rotated->left->left->parent->value, $rotated->left->value);
+    }
+
+    /**
+     *
+     */
+    public function testInsertOld()
+    {
+        $avl = new AVLTree();
 
         $avl->insert(7);
         $avl->insert(3);
@@ -77,6 +267,6 @@ class AVLTreeTest extends BaseBSTTest
         $avl->insert(6);
         $avl->insert(9);
         $avl->insert(33);
-        $avl->preOrder($avl->getRoot());
+        $avl->printTree();
     }
 }
