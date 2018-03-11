205895f9d0906bf2b1069256d7cc1c8295e1c649
Renato Soares <renatosoarespro@hotmail.com>
2018-03-11 11:39:47 -0300

    test unit AVL tree insert value

--- tests/AVLTreeTest.php
+++ tests/AVLTreeTest.php
@@ -29,6 +29,39 @@ class AVLTreeTest extends BaseBSTTest
         $this->testTreeBSTProperties($tree->root);
     }
 
+    public function testInsert()
+    {
+        /** @var AVLTree $tree */
+        $tree = new AVLTree();
+        $tree->insert(20);
+        $tree->insert(15);
+        $tree->insert(25);
+        $tree->insert(22);
+        $tree->insert(21);
+        $this->assertEquals($tree->getSize(), 5);
+        $this->assertEquals((int) $tree->root->value, 20);
+        $this->assertEquals((int) $tree->root->left->value, 15);
+
+        /**
+         * @var AVLNode rightSubtree
+         * @vat AVLTree $tree->root->right
+         */
+        $rightSubtree = $tree->root->right;
+        // Rotação realizada e altura + balanço atualizado
+        $this->assertEquals((int) $rightSubtree->value, 22);
+        $this->assertEquals((int) $rightSubtree->height, 1);
+        $this->assertEquals((int) $rightSubtree->right->value, 25);
+
+        /** @var AVLNode $rightSubtree->right */
+        $this->assertEquals(($rightSubtree->right)->height, 0);
+        $this->assertEquals((int) $rightSubtree->left->value, 21);
+
+        /** @var AVLNode $rightSubtree->left */
+        $this->assertEquals(($rightSubtree->left)->height, 0);
+
+        $this->testTreeBSTProperties($tree->root);
+    }
+
     public function testInsertTree()
     {
         $avl = new AvlTree();
